d040598988dc707edf666840a2ea6509d591f11a
Russ Ross <russ@russross.com>
2023-10-31 17:20:38 -0600

    solver helpers and printer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Debug, Clone)]
 struct Input {
     name: String,
@@ -208,7 +210,7 @@ impl Input {
                     let Ok(time_slot) = self.find_time_slot_by_name(&time_name) else {
                         return Err(format!("unknown time slot name {}", time_name).into());
                     };
-                    if times.iter().any(|t| t.time == time_slot) {
+                    if times.iter().any(|t| t.time_slot == time_slot) {
                         return Err(format!(
                             "time slot {} appears twice for instructor {}",
                             time_name, name
@@ -216,7 +218,7 @@ impl Input {
                         .into());
                     }
                     times.push(TimeWithPenalty {
-                        time: time_slot,
+                        time_slot: time_slot,
                         penalty: badness,
                     });
                 }
@@ -313,7 +315,7 @@ impl Input {
                         add_time_with_penalty_keep_worst(
                             &mut twp,
                             TimeWithPenalty {
-                                time: time_i,
+                                time_slot: time_i,
                                 penalty: badness,
                             },
                         );
@@ -351,13 +353,13 @@ impl Input {
         // calculate badness for each room/time and filter by
         // instructor availability (including badness)
         let mut rtp = Vec::new();
-        'rt: for time in twp.iter() {
-            let mut time_penalty = time.penalty;
+        'rt: for time_slot in twp.iter() {
+            let mut time_penalty = time_slot.penalty;
             for instructor_i in instructors.iter() {
                 match self.instructors[*instructor_i]
                     .available_times
                     .iter()
-                    .find(|itwp| itwp.time == time.time)
+                    .find(|itwp| itwp.time_slot == time_slot.time_slot)
                 {
                     Some(itwp) => time_penalty = std::cmp::max(time_penalty, itwp.penalty),
                     None => continue 'rt,
@@ -366,7 +368,7 @@ impl Input {
             for room in rwp.iter() {
                 rtp.push(RoomTimeWithPenalty {
                     room: room.room,
-                    time: time.time,
+                    time_slot: time_slot.time_slot,
                     penalty: std::cmp::min(99, room.penalty + time_penalty),
                 });
             }
@@ -597,7 +599,7 @@ fn add_room_with_penalty_keep_worst(list: &mut Vec<RoomWithPenalty>, rwp: RoomWi
 }
 
 fn add_time_with_penalty_keep_worst(list: &mut Vec<TimeWithPenalty>, twp: TimeWithPenalty) {
-    match list.iter().position(|elt| elt.time == twp.time) {
+    match list.iter().position(|elt| elt.time_slot == twp.time_slot) {
         Some(i) => list[i].penalty = std::cmp::max(list[i].penalty, twp.penalty),
         None => list.push(twp),
     }
@@ -632,7 +634,7 @@ struct RoomTime {
 
 #[derive(Debug, Clone)]
 struct TimeWithPenalty {
-    time: usize,
+    time_slot: usize,
     penalty: u16,
 }
 
@@ -645,7 +647,7 @@ struct RoomWithPenalty {
 #[derive(Debug, Clone, Eq, PartialEq, Ord)]
 struct RoomTimeWithPenalty {
     room: usize,
-    time: usize,
+    time_slot: usize,
     penalty: u16,
 }
 
@@ -654,8 +656,8 @@ impl PartialOrd for RoomTimeWithPenalty {
         if self.room != other.room {
             return Some(self.room.cmp(&other.room));
         }
-        if self.time != other.time {
-            return Some(self.time.cmp(&other.time));
+        if self.time_slot != other.time_slot {
+            return Some(self.time_slot.cmp(&other.time_slot));
         }
         return Some(self.penalty.cmp(&other.penalty));
     }
@@ -879,11 +881,11 @@ fn main() -> Result<(), Box<rhai::EvalAltResult>> {
     for inst_i in instructor_order {
         let inst = &term.instructors[inst_i];
         print!("{}", inst.name);
-        for time in &inst.available_times {
-            if time.penalty == 0 {
-                print!(" {}", term.time_slots[time.time].name);
+        for twp in &inst.available_times {
+            if twp.penalty == 0 {
+                print!(" {}", term.time_slots[twp.time_slot].name);
             } else {
-                print!(" {}:{}", term.time_slots[time.time].name, time.penalty);
+                print!(" {}:{}", term.time_slots[twp.time_slot].name, twp.penalty);
             }
         }
         println!("");
@@ -922,7 +924,7 @@ fn main() -> Result<(), Box<rhai::EvalAltResult>> {
                 prev_room = rtp.room;
                 print!("\n    {}:", &term.rooms[rtp.room].name);
             }
-            print!(" {}", &term.time_slots[rtp.time].name);
+            print!(" {}", &term.time_slots[rtp.time_slot].name);
             if rtp.penalty > 0 {
                 print!(":{}", rtp.penalty);
             }
@@ -958,6 +960,21 @@ fn main() -> Result<(), Box<rhai::EvalAltResult>> {
         }
         println!("");
     }
+
+    let mut solver = Solver::new(&term);
+    let mut rng = rand::thread_rng();
+    for _ in 0..10000 {
+        let section_i = rng.gen_range(0..term.sections.len());
+        let section = &term.sections[section_i];
+        if section.cross_listings[0] != section_i {
+            continue;
+        }
+        let &RoomTimeWithPenalty{room, time_slot, ..} = &section.room_times[rng.gen_range(0..section.room_times.len())];
+        solver.remove_placement(section_i);
+        solver.displace_conflicts(&term, section_i, room, time_slot);
+        solver.add_placement(section_i, room, time_slot);
+    }
+    solver.print_schedule(&term);
 
     Ok(())
 }
@@ -1008,39 +1025,149 @@ impl Solver {
         }
     }
 
+    fn add_placement(&mut self, section: usize, room: usize, time_slot: usize) {
+        if let Some(RoomTime {
+            room: _r,
+            time_slot: _t,
+        }) = std::mem::replace(
+            &mut self.sections[section].placement,
+            Some(RoomTime { room, time_slot }),
+        ) {
+            panic!("Solver::add_placement section already filled");
+        }
+        if let Some(_s) = std::mem::replace(
+            &mut self.room_placements[room].time_slot_placements[time_slot],
+            Some(section),
+        ) {
+            panic!("Solver::add_placement room time pair already filled");
+        }
+    }
+
     // remove any sections that will be in conflict with a section about to be placed
     //
     // this includes:
     // * anything in the same room in an overlapping time slot
     // * anything in the hard conflict list of this section (or a cross listing)
     //   in the same/an overlapping time slot
-    fn displace_conflict(&mut self, input: &Input, section: usize, room: usize, time_slot: usize) {
+    fn displace_conflicts(&mut self, input: &Input, section: usize, room: usize, time_slot: usize) {
         // is this slot (or an overlapping time in the same room) already occupied?
+        let mut evictees = Vec::new();
         for overlapping in &input.time_slots[time_slot].conflicts {
             if let Some(existing) = self.room_placements[room].time_slot_placements[*overlapping] {
-                self.remove_placement(existing);
+                evictees.push(existing);
             }
         }
-        //cases:
-        //any overlapping time in self? weird case;
-        //same room & overlapping time,
-        //hard conflict courses from conflict list
-        let existing =
-            std::mem::take(&mut self.room_placements[room].time_slot_placements[time_slot]);
-        if let Some(other) = existing {
-            let section_placement = std::mem::take(&mut self.sections[other].placement);
 
-            // sanity check
-            let Some(RoomTime {
-                room: other_room,
-                time_slot: other_time_slot,
-            }) = section_placement
-            else {
-                panic!("Solver::displace_conflict room placement found but solver section is None");
-            };
-            if other_room != room || other_time_slot != time_slot {
-                panic!("Solver::displace_conflict room placement does not match solver section");
-            };
+        for cross_listing in &input.sections[section].cross_listings {
+            for hard_conflict in &input.sections[*cross_listing].hard_conflicts {
+                let main_cross_listing = input.sections[*hard_conflict].cross_listings[0];
+                if let Some(RoomTime {
+                    room: _r,
+                    time_slot: t,
+                }) = self.sections[main_cross_listing].placement
+                {
+                    if input.time_slots[time_slot].conflicts.contains(&t) {
+                        evictees.push(main_cross_listing);
+                    }
+                }
+            }
         }
+
+        evictees.iter().for_each(|&i| self.remove_placement(i));
+    }
+
+    fn select_section_to_place(&mut self, input: &Input) {
+        let mut rng = rand::thread_rng();
+        let selection = rng.gen_range(0..input.sections.len());
+    }
+
+    fn print_schedule(&self, input: &Input) {
+        let mut name_len = 0;
+        for (instructor_i, instructor) in input.instructors.iter().enumerate() {
+            for &section_i in &instructor.sections {
+                let section = &input.sections[section_i];
+                if section.cross_listings.len() > 1 {
+                    name_len = std::cmp::max(name_len, section.course.len() + section.section.len() + 2);
+                } else {
+                    name_len = std::cmp::max(name_len, section.course.len() + section.section.len() + 1);
+                }
+                if section.instructors[0] == instructor_i {
+                    if section.instructors.len() > 1 {
+                        name_len = std::cmp::max(name_len, instructor.name.len()+1);
+                    } else {
+                        name_len = std::cmp::max(name_len, instructor.name.len());
+                    }
+                }
+            }
+        }
+
+        let mut room_len = 0;
+        for room in &input.rooms {
+            room_len = std::cmp::max(room_len, room.name.len());
+        }
+        name_len = std::cmp::max(name_len, room_len);
+
+        let mut time_len = 0;
+        for time_slot in &input.time_slots {
+            time_len = std::cmp::max(time_len, time_slot.name.len());
+        }
+
+        // print the top row labels
+        print!("{:time_len$} ", "");
+        for room in &input.rooms {
+            print!("  {:^width$} ", room.name, width = name_len);
+        }
+        println!("");
+
+        // loop over time slots
+        for (time_slot_i, time_slot) in input.time_slots.iter().enumerate() {
+            // top line
+            print!("{:time_len$} ", "");
+            for _ in 0..input.rooms.len() {
+                print!("+-{:-<name_len$}-", "");
+            }
+            println!("+");
+
+            // instructor line
+            print!("{:time_len$} ", time_slot.name);
+            for room_i in 0..input.rooms.len() {
+                if let Some(section_i) = self.room_placements[room_i].time_slot_placements[time_slot_i] {
+                    let instructors = &input.sections[section_i].instructors;
+                    let name = &input.instructors[instructors[0]].name;
+                    if instructors.len() > 1 {
+                        print!("| {:<width$}+ ", name, width=name_len-1);
+                    } else {
+                        print!("| {:<width$} ", name, width=name_len);
+                    }
+                } else {
+                    print!("| {:name_len$} ", "");
+                }
+            }
+            println!("|");
+
+            // course line
+            print!("{:time_len$} ", "");
+            for room_i in 0..input.rooms.len() {
+                if let Some(section_i) = self.room_placements[room_i].time_slot_placements[time_slot_i] {
+                    let section = &input.sections[input.sections[section_i].cross_listings[0]];
+                    let name = format!("{}-{}", section.course, section.section);
+                    if input.sections[section_i].cross_listings.len() > 1 {
+                        print!("| {:<width$}+ ", name, width=name_len-1);
+                    } else {
+                        print!("| {:<width$} ", name, width=name_len);
+                    }
+                } else {
+                    print!("| {:name_len$} ", "");
+                }
+            }
+            println!("|");
+        }
+
+        // bottom line
+        print!("{:time_len$} ", "");
+        for _ in 0..input.rooms.len() {
+            print!("+-{:-<name_len$}-", "");
+        }
+        println!("+");
     }
 }
